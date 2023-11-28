cefa346de6c2d601cc9c3cc2c6a9e60aee59af25
1shabbirhussain <001.shabbirhussain@gmail.com>
2023-11-29 00:10:37 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/avildo_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.avildo_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:avildo_test/Screens/product_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProductView() 
+    );
+  }
+}
